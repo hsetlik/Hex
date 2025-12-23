7342f20aa93eaceb9de055bf7b644d2a0ef99f8b
hsetlik <haydensetlik@gmail.com>
2025-12-23 12:36:13 -0700

    Asset loading code

--- plugin/include/Hex/GUI/Assets.h
+++ plugin/include/Hex/GUI/Assets.h
@@ -1,1 +1,12 @@
-# pragma once
+#pragma once
+#include "HexHeader.h"
+#include "Color.h"
+
+namespace Assets {
+enum AssetID { KnobTop, KnobBkgnd, Thumb, PlaybackOn, PlaybackOff };
+juce::Image& getImage(const AssetID& id);
+
+enum WaveID { Sine, Tri, Saw, Square, Noise };
+
+juce::Image& getWaveImage(const WaveID& id, bool isOn);
+}  // namespace Assets
--- plugin/source/Assets.cpp
+++ plugin/source/Assets.cpp
@@ -1,1 +1,108 @@
+#include "GUI/Assets.h"
+#include "AssetData.h"
+
+namespace Assets {
+
+juce::Image& getImage(const AssetID& id) {
+  static juce::Image iKnobTop = juce::ImageFileFormat::loadFrom(
+      AssetData::KnobTop_48x48_png, AssetData::KnobTop_48x48_pngSize);
+  static juce::Image iKnobBkgnd = juce::ImageFileFormat::loadFrom(
+      AssetData::KnobBkgnd_70x70_png, AssetData::KnobBkgnd_70x70_pngSize);
+  static juce::Image iThumb = juce::ImageFileFormat::loadFrom(
+      AssetData::Thumb_84x60_png, AssetData::Thumb_84x60_pngSize);
+  static juce::Image iPlaybackOn = juce::ImageFileFormat::loadFrom(
+      AssetData::PlaybackOn_100x100_png, AssetData::PlaybackOn_100x100_pngSize);
+  static juce::Image iPlaybackOff =
+      juce::ImageFileFormat::loadFrom(AssetData::PlaybackOff_100x100_png,
+                                      AssetData::PlaybackOff_100x100_pngSize);
+  switch (id) {
+    case KnobTop:
+      return iKnobTop;
+      break;
+    case KnobBkgnd:
+      return iKnobBkgnd;
+      break;
+    case Thumb:
+      return iThumb;
+      break;
+    case PlaybackOn:
+      return iPlaybackOn;
+      break;
+    case PlaybackOff:
+      return iPlaybackOff;
+      break;
+    default:
+      jassert(false);
+      return iThumb;
+  }
+}
+
+juce::Image& getWaveImage(const WaveID& id, bool isOn) {
+  // on images----
+  static juce::Image iSineOn = juce::ImageFileFormat::loadFrom(
+      AssetData::SineOn_72x72_png, AssetData::SineOn_72x72_pngSize);
+  static juce::Image iTriOn = juce::ImageFileFormat::loadFrom(
+      AssetData::TriOn_72x72_png, AssetData::TriOn_72x72_pngSize);
+  static juce::Image iSawOn = juce::ImageFileFormat::loadFrom(
+      AssetData::SawOn_72x72_png, AssetData::SawOn_72x72_pngSize);
+  static juce::Image iSquareOn = juce::ImageFileFormat::loadFrom(
+      AssetData::SquareOn_72x72_png, AssetData::SquareOn_72x72_pngSize);
+  static juce::Image iNoiseOn = juce::ImageFileFormat::loadFrom(
+      AssetData::NoiseOn_72x72_png, AssetData::NoiseOn_72x72_pngSize);
+  // off images----
+  static juce::Image iSineOff = juce::ImageFileFormat::loadFrom(
+      AssetData::SineOff_72x72_png, AssetData::SineOff_72x72_pngSize);
+  static juce::Image iTriOff = juce::ImageFileFormat::loadFrom(
+      AssetData::TriOff_72x72_png, AssetData::TriOff_72x72_pngSize);
+  static juce::Image iSawOff = juce::ImageFileFormat::loadFrom(
+      AssetData::SawOff_72x72_png, AssetData::SawOff_72x72_pngSize);
+  static juce::Image iSquareOff = juce::ImageFileFormat::loadFrom(
+      AssetData::SquareOff_72x72_png, AssetData::SquareOff_72x72_pngSize);
+  static juce::Image iNoiseOff = juce::ImageFileFormat::loadFrom(
+      AssetData::NoiseOff_72x72_png, AssetData::NoiseOff_72x72_pngSize);
+  if (isOn) {
+    switch (id) {
+      case Sine:
+        return iSineOn;
+        break;
+      case Tri:
+        return iTriOn;
+        break;
+      case Saw:
+        return iSawOn;
+        break;
+      case Square:
+        return iSquareOn;
+        break;
+      case Noise:
+        return iNoiseOn;
+        break;
+      default:
+        jassert(false);
+        return iSineOn;
+    }
+  } else {
+    switch (id) {
+      case Sine:
+        return iSineOff;
+        break;
+      case Tri:
+        return iTriOff;
+        break;
+      case Saw:
+        return iSawOff;
+        break;
+      case Square:
+        return iSquareOff;
+        break;
+      case Noise:
+        return iNoiseOff;
+        break;
+      default:
+        jassert(false);
+        return iSineOff;
+    }
+  }
+}
+}  // namespace Assets
 //===================================================
